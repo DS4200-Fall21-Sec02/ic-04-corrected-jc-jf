9735f032befb11e245e3afba7ff51968f6d4ed9c
jdflodman <57110574+jdflodman@users.noreply.github.com>
2021-10-12 13:25:57 -0400

    Final push

--- js/main.js
+++ js/main.js
@@ -22,10 +22,59 @@ let svg1 = d3.select('#d3-container')
 
 d3.csv("data/data.csv").then(function(data){
 
+// add x scale
 var xScale = d3.scaleBand()
   .range ([0, width])
   .domain (data.map(d => d.X));
 svg1.append("g")
-  .attr("transform", 'translate(0, ' + height +')')
+  .attr("transform", 'translate(' + margin.left + ',' + (height+30) +')')
   .call(d3.axisBottom(xScale));
+
+
+// add y scale
+let maxY = d3.max(data, function(d) {return d.Y;})
+let yScale = d3.scaleLinear()
+                .domain([0, maxY])
+                .range([height, 0]);
+
+// add y axis to SVG
+  svg1.append("g")
+  .attr("transform", "translate(" + margin.left + ",30)")
+  .call(d3.axisLeft(yScale));
+
+//create popup with information
+var Tooltip = d3.select("#d3-container")
+                .append("div")
+                .style("opacity", 0)
+                .attr("class", "tooltip")
+                .attr('style', 'position: absolute');
+
+// add bars
+svg1.selectAll('.bar')
+  .data(data)
+  .enter().append('rect')
+  .attr('class', 'bar')
+  .attr('x', function(d) {return xScale(d.X) + margin.left + 10})
+  .attr('y', function(d) {return yScale(d.Y) + margin.top-30})
+  .attr('width', xScale.bandwidth() - 20)
+  .attr('height', function(d) {return height - yScale(d.Y);})
+  .attr('fill', 'orange')
+  .on('mouseover', function(d, i) {
+          // information popup becomes visible
+          Tooltip.transition().duration(200).style("opacity", .9);
+          Tooltip.html("<b>" + i.X + "</b><br>"+ i.Y)
+                  .style("left", d.pageX - 20 + "px")
+                  .style("top", d.pageY - 60 + "px");
+
+        })
+        // trigger mouse-leaving event
+  .on('mouseout', function(d) {
+    //decrease blob size
+    d3.select(this).transition()
+    // popup disappears  
+    Tooltip.transition().duration(500).style("opacity", 0);
+  });;
+
+
+
 }) 
